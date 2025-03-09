6762dec8eaf07b5601e4f4150fb69e4e8c247be4
Sai Ashirwad <saiashirwadr@gmail.com>
2025-03-10 00:49:33 +0530

    wip

--- index.ts
+++ index.ts
@@ -1,7 +1,48 @@
-//type ColumnType = "string" | "number" | "boolean" | "date" | "json";
+type Clean<T> = { [k in keyof T]: T[k] } & unknown;
 
-abstract class BaseColumn<T> {
-	constructor(public readonly type: string) {}
+type BaseColumnOptions<T> = {
+	type: string;
+	__nullable?: boolean;
+	__default?: T;
+};
+
+type BaseColumn<T, options extends BaseColumnOptions<T>> = {
+	options: BaseColumnOptions<T>;
+	nullable: () => BaseColumn<T, Clean<options & { __nullable: true }>>;
+	default: (value: T) => BaseColumn<T, Clean<options & { __default: T }>>;
+};
+
+function BaseColumn<const type extends string, T = unknown>(
+	type: type,
+): BaseColumn<T, { type: type }> {
+	const options: BaseColumnOptions<T> = { type };
+	return {
+		options,
+		nullable() {
+			return {
+				...this,
+				options: { ...options, __nullable: true },
+			};
+		},
+		default<U extends T>(value: U) {
+			return {
+				...this,
+				options: { ...options, __default: value },
+			};
+		},
+	};
 }
-class Column<T> {}
 
+function StringColumn() {
+	return { ...BaseColumn<"string", string>("string") };
+}
+
+function NumberColumn() {
+	return { ...BaseColumn<"number", number>("number") };
+}
+
+const nullableName = StringColumn().nullable();
+console.log(nullableName);
+
+const name = StringColumn().nullable().default("5");
+console.log(name);
