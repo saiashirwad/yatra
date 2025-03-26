ee0c10ed7944bfe0f4ee6524ef882eb433d0deaf
Sai Ashirwad <saiashirwadr@gmail.com>
2025-03-26 12:12:52 +0530

    moved columns-new to use classes

--- src/columns-new.ts
+++ src/columns-new.ts
@@ -93,233 +93,263 @@ export type ColumnType =
   | "decimal"
   | "enum";
 
-interface BaseColumn<CT extends ColumnType, DT extends any>
-  extends Pipeable.Pipeable
+// interface Column<CT extends ColumnType, DT extends any>
+//   extends Pipeable.Pipeable
+// {
+//   [Type]: CT;
+//   [DataType]: DT;
+// }
+
+// function Column<CT extends ColumnType, DT extends any>(
+//   type: CT,
+// ): Column<CT, DT> {
+//   return {
+//     [DataType]: null as any,
+//     [Type]: type,
+//     pipe() {
+//       return Pipeable.pipeArguments(this, arguments);
+//     },
+//   };
+// }
+
+class Column<CT extends ColumnType, DT extends any>
+  implements Pipeable.Pipeable
 {
-  [Type]: CT;
-  [DataType]: DT;
+  readonly [Type]: CT;
+  declare readonly [DataType]: DT;
+
+  constructor(type: CT) {
+    this[Type] = type;
+    this[DataType] = null as any;
+  }
+
+  pipe(...fns: Array<Function>) {
+    return Pipeable.pipeArguments(this, arguments);
+  }
 }
 
-function makeColumn<CT extends ColumnType, DT extends any>(
-  type: CT,
-): BaseColumn<CT, DT> {
-  return {
-    [DataType]: null as any,
-    [Type]: type,
-    pipe() {
-      return Pipeable.pipeArguments(this, arguments);
-    },
-  };
-}
-
-interface StringColumn extends BaseColumn<"string", string> {}
-interface NumberColumn extends BaseColumn<"number", number> {}
-interface BooleanColumn extends BaseColumn<"boolean", boolean> {}
-interface DateColumn extends BaseColumn<"date", Date | number> {}
-interface JsonColumn extends BaseColumn<"json", object | string> {}
-interface JsonbColumn extends BaseColumn<"jsonb", object | string> {}
-interface UuidColumn extends BaseColumn<"uuid", string> {}
-interface TextColumn extends BaseColumn<"text", string> {}
-interface BigIntColumn extends BaseColumn<"bigint", bigint | number> {}
-interface TimestampColumn
-  extends BaseColumn<"timestamp", Date | string | number>
-{}
-interface TimeColumn extends BaseColumn<"time", string | Date> {}
-interface BinaryColumn
-  extends BaseColumn<"binary", Uint8Array | Buffer | string>
-{}
-interface DecimalColumn extends BaseColumn<"decimal", string | number> {}
+interface StringColumn extends Column<"string", string> {}
+interface NumberColumn extends Column<"number", number> {}
+interface BooleanColumn extends Column<"boolean", boolean> {}
+interface DateColumn extends Column<"date", Date | number> {}
+interface JsonColumn extends Column<"json", object | string> {}
+interface JsonbColumn extends Column<"jsonb", object | string> {}
+interface UuidColumn extends Column<"uuid", string> {}
+interface TextColumn extends Column<"text", string> {}
+interface BigIntColumn extends Column<"bigint", bigint | number> {}
+interface TimestampColumn extends Column<"timestamp", Date | string | number> {}
+interface TimeColumn extends Column<"time", string | Date> {}
+interface BinaryColumn extends Column<"binary", Uint8Array | Buffer | string> {}
+interface DecimalColumn extends Column<"decimal", string | number> {}
 
 const LiteralValue = Symbol.for("LiteralValue");
 interface LiteralColumn<T extends string | number | boolean>
-  extends BaseColumn<"literal", T>
+  extends Column<"literal", T>
 {
   readonly [LiteralValue]: T;
 }
 
 const ItemType = Symbol.for("ItemType");
-interface ArrayColumn<ItemType extends BaseColumn<any, any>>
-  extends BaseColumn<"array", Array<ItemType[typeof DataType]>>
+interface ArrayColumn<ItemType extends Column<any, any>>
+  extends Column<"array", Array<ItemType[typeof DataType]>>
 {
   readonly [ItemType]: ItemType;
 }
 
 const EnumValues = Symbol.for("EnumValues");
-interface EnumColumn<T extends string[]> extends BaseColumn<"enum", T[number]> {
+interface EnumColumn<T extends string[]> extends Column<"enum", T[number]> {
   readonly [EnumValues]: T;
 }
 
-export function string(): StringColumn {
-  return makeColumn("string");
-}
-
-export function number(): NumberColumn {
-  return makeColumn("number");
-}
-
-export function boolean(): BooleanColumn {
-  return makeColumn("boolean");
-}
-
-export function date(): DateColumn {
-  return makeColumn("date");
-}
-
-export function json(): JsonColumn {
-  return makeColumn("json");
-}
-
-export function jsonb(): JsonbColumn {
-  return makeColumn("jsonb");
-}
-
-export function uuid(): UuidColumn {
-  return makeColumn("uuid");
-}
-
-export function text(): TextColumn {
-  return makeColumn("text");
-}
-
-export function bigint(): BigIntColumn {
-  return makeColumn("bigint");
-}
-
-export function timestamp(withTimezone: boolean = true): TimestampColumn {
-  const col = makeColumn<"timestamp", Date | string | number>("timestamp");
-  return extend(col, "withTimezone", withTimezone);
-}
-
-export function time(withTimezone: boolean = false): TimeColumn {
-  const col = makeColumn<"time", string | Date>("time");
-  return extend(col, "withTimezone", withTimezone);
-}
-
-export function binary(): BinaryColumn {
-  return makeColumn("binary");
-}
-
-export function decimal(): DecimalColumn {
-  return makeColumn("decimal");
-}
-
-export function literal<T extends string | number | boolean>(
-  value: T,
-): LiteralColumn<T> {
-  const col = makeColumn<"literal", T>("literal");
-  return extend(col, LiteralValue, value) as any;
-}
-
-export function array<T extends BaseColumn<any, any>>(
-  itemType: T,
-): ArrayColumn<T> {
-  const col = makeColumn<"array", Array<T[typeof DataType]>>("array");
-  return extend(col, ItemType, itemType) as any;
-}
-
-export function enum_<T extends string[]>(values: T): EnumColumn<T> {
-  const col = makeColumn<"enum", T[number]>("enum");
-  return extend(col, EnumValues, values) as any;
-}
-
-// Column modifiers
-export const nullable = <Col extends BaseColumn<any, any>>(c: Col) =>
-  extend<Col, Nullable>(c, Nullable, true);
-
-export const unique = <Col extends BaseColumn<any, any>>(c: Col) =>
-  extend<Col, Unique>(c, Unique, true);
-
-export const primaryKey = <Col extends BaseColumn<any, any>>(c: Col) =>
-  extend<Col, PrimaryKey>(c, PrimaryKey, true);
-
-export const defaultValue = <
-  Col extends BaseColumn<any, any>,
-  const V extends Col[typeof DataType],
->(value: V) =>
-(c: Col) => extend<Col, Default<V>>(c, Default, value);
-
-export const columnName = <
-  Col extends BaseColumn<any, any>,
-  const ColName extends string,
->(name: ColName) =>
-(c: Col) => extend<Col, ColumnName<ColName>>(c, ColumnName, name);
-
 export const minLength = <
-  Col extends BaseColumn<"string", any>,
+  Col extends Column<"string", any>,
   const MinL extends number,
 >(length: MinL) =>
 (c: Col) => extend<Col, MinLength<MinL>>(c, MinLength, length);
 
 export const maxLength = <
-  Col extends BaseColumn<"string", any>,
+  Col extends Column<"string", any>,
   const MaxL extends number,
 >(length: MaxL) =>
 (c: Col) => extend<Col, MaxLength<MaxL>>(c, MaxLength, length);
 
+class StringColumn extends Column<"string", string> {
+  constructor() {
+    super("string");
+  }
+
+  minLength<T extends number>(
+    length: T,
+  ): this & MinLength<T> {
+    return pipe(this, minLength(length));
+  }
+
+  maxLength<T extends number>(
+    length: T,
+  ): this & MaxLength<T> {
+    return pipe(this, maxLength(length));
+  }
+}
+
+export function string(): StringColumn {
+  return new StringColumn();
+}
+
+export function number(): NumberColumn {
+  return new Column("number");
+}
+
+export function boolean(): BooleanColumn {
+  return new Column("boolean");
+}
+
+export function date(): DateColumn {
+  return new Column("date");
+}
+
+export function json(): JsonColumn {
+  return new Column("json");
+}
+
+export function jsonb(): JsonbColumn {
+  return new Column("jsonb");
+}
+
+export function uuid(): UuidColumn {
+  return new Column("uuid");
+}
+
+export function text(): TextColumn {
+  return new Column("text");
+}
+
+export function bigint(): BigIntColumn {
+  return new Column("bigint");
+}
+
+export function timestamp(withTimezone: boolean = true): TimestampColumn {
+  const col = new Column<"timestamp", Date | string | number>("timestamp");
+  return extend(col, "withTimezone", withTimezone);
+}
+
+export function time(withTimezone: boolean = false): TimeColumn {
+  const col = new Column<"time", string | Date>("time");
+  return extend(col, "withTimezone", withTimezone);
+}
+
+export function binary(): BinaryColumn {
+  return new Column("binary");
+}
+
+export function decimal(): DecimalColumn {
+  return new Column("decimal");
+}
+
+export function literal<T extends string | number | boolean>(
+  value: T,
+): LiteralColumn<T> {
+  const col = new Column<"literal", T>("literal");
+  return extend(col, LiteralValue, value) as any;
+}
+
+export function array<T extends Column<any, any>>(
+  itemType: T,
+): ArrayColumn<T> {
+  const col = new Column<"array", Array<T[typeof DataType]>>("array");
+  return extend(col, ItemType, itemType) as any;
+}
+
+export function enum_<T extends string[]>(values: T): EnumColumn<T> {
+  const col = new Column<"enum", T[number]>("enum");
+  return extend(col, EnumValues, values) as any;
+}
+
+// Column modifiers
+export const nullable = <Col extends Column<any, any>>(c: Col) =>
+  extend<Col, Nullable>(c, Nullable, true);
+
+export const unique = <Col extends Column<any, any>>(c: Col) =>
+  extend<Col, Unique>(c, Unique, true);
+
+export const primaryKey = <Col extends Column<any, any>>(c: Col) =>
+  extend<Col, PrimaryKey>(c, PrimaryKey, true);
+
+export const defaultValue = <
+  Col extends Column<any, any>,
+  const V extends Col[typeof DataType],
+>(value: V) =>
+(c: Col) => extend<Col, Default<V>>(c, Default, value);
+
+export const columnName = <
+  Col extends Column<any, any>,
+  const ColName extends string,
+>(name: ColName) =>
+(c: Col) => extend<Col, ColumnName<ColName>>(c, ColumnName, name);
+
 export const format = <
-  Col extends BaseColumn<any, any>,
+  Col extends Column<any, any>,
   const Fmt extends string,
 >(fmt: Fmt) =>
 (c: Col) => extend<Col, Format<Fmt>>(c, Format, fmt);
 
 export const min = <
-  Col extends BaseColumn<"number", any>,
+  Col extends Column<"number", any>,
   const M extends number,
 >(minValue: M) =>
 (c: Col) => extend<Col, Min<M>>(c, Min, minValue);
 
 export const max = <
-  Col extends BaseColumn<"number", any>,
+  Col extends Column<"number", any>,
   const M extends number,
 >(maxValue: M) =>
 (c: Col) => extend<Col, Max<M>>(c, Max, maxValue);
 
-export const integer = <Col extends BaseColumn<"number", any>>(c: Col) =>
+export const integer = <Col extends Column<"number", any>>(c: Col) =>
   extend<Col, Integer>(c, Integer, true);
 
 export const references = <
-  Col extends BaseColumn<any, any>,
+  Col extends Column<any, any>,
   const T extends string,
 >(table: T, column: string) =>
 (c: Col) => extend<Col, References<T>>(c, References, { table, column });
 
-export const autoIncrement = <Col extends BaseColumn<any, any>>(c: Col) =>
+export const autoIncrement = <Col extends Column<any, any>>(c: Col) =>
   extend<Col, AutoIncrement>(c, AutoIncrement, true);
 
 export const generated = <
-  Col extends BaseColumn<any, any>,
+  Col extends Column<any, any>,
   const T extends string,
 >(expression: T) =>
 (c: Col) => extend<Col, Generated<T>>(c, Generated, { expression });
 
 export const comment = <
-  Col extends BaseColumn<any, any>,
+  Col extends Column<any, any>,
   const T extends string,
 >(text: T) =>
 (c: Col) => extend<Col, Comment<T>>(c, Comment, text);
 
 export const precision = <
-  Col extends BaseColumn<"number" | "decimal", any>,
+  Col extends Column<"number" | "decimal", any>,
   const T extends number,
 >(value: T) =>
 (c: Col) => extend<Col, Precision<T>>(c, Precision, value);
 
 export const scale = <
-  Col extends BaseColumn<"number" | "decimal", any>,
+  Col extends Column<"number" | "decimal", any>,
   const T extends number,
 >(value: T) =>
 (c: Col) => extend<Col, Scale<T>>(c, Scale, value);
 
 export const check = <
-  Col extends BaseColumn<any, any>,
+  Col extends Column<any, any>,
   const T extends string,
 >(expression: T) =>
 (c: Col) => extend<Col, Check<T>>(c, Check, expression);
 
-export const index = <Col extends BaseColumn<any, any>>(c: Col) =>
+export const index = <Col extends Column<any, any>>(c: Col) =>
   extend<Col, Index>(c, Index, true);
 
-export const searchIndexed = <Col extends BaseColumn<any, any>>(c: Col) =>
+export const searchIndexed = <Col extends Column<any, any>>(c: Col) =>
   extend<Col, SearchIndexed>(c, SearchIndexed, true);
 
 export type IsPrimaryKey<T> = T extends PrimaryKey ? true
@@ -355,8 +385,7 @@ export type GetMinLength<T> = T extends MinLength<infer V> ? V
 export type GetMaxLength<T> = T extends MaxLength<infer V> ? V
   : undefined;
 
-export type GetDataType<T> = T extends BaseColumn<any, infer DataType>
-  ? DataType
+export type GetDataType<T> = T extends Column<any, infer DataType> ? DataType
   : never;
 
 export type GetColumnType<T> = T extends StringColumn ? string
@@ -382,6 +411,7 @@ const userNameColumn = pipe(
   minLength(3),
   maxLength(50),
   unique,
+  nullable,
 );
 
 const idColumn = pipe(
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,11 @@
-import { pipe, Pipeable } from "effect";
+import { Schema } from "effect";
 
-interface Something<S> extends Pipeable.Pipeable {
-  str: S;
-  map: <T>(fn: (s: S) => T) => Something<T>;
-}
+class Lol extends Schema.Class<Lol>("Lol")({
+  name: Schema.String,
+}) {}
 
-function makeSomething<const S>(str: S): Something<S> {
-  return {
-    str,
-    map: (fn) => makeSomething(fn(str)),
-    pipe() {
-      return Pipeable.pipeArguments(this, arguments);
-    },
-  } as Something<S>;
-}
+console.log(Lol);
 
-type MapFn<S, T> = (s: S) => T;
+const lol = new Lol({ name: "hi" });
 
-function map<const S, const T>(
-  fn: MapFn<S, T>,
-): (something: Something<S>) => Something<T> {
-  return (something: Something<S>) => makeSomething(fn(something.str));
-}
-
-type UppercaseEverything<S> = S extends string ? Uppercase<S>
-  : S extends Record<any, any>
-    ? { [K in keyof S as K extends string ? Uppercase<K> : K]: Uppercase<S[K]> }
-  : never;
-
-const uppercase = <const S extends string | Record<string, string>>() =>
-  map<S, UppercaseEverything<S>>(s => {
-    if (typeof s === "object") {
-      const result = {} as any;
-      for (const key in s) {
-        result[key.toUpperCase() as any] = (s[key] as string).toUpperCase();
-      }
-      return result;
-    }
-    if (typeof s === "string") {
-      return s.toUpperCase() as any;
-    }
-  });
-
-const something = makeSomething({ hi: "there" });
-
-const result = something.pipe(
-  map(s => ({ ...s, lol: "wut" })),
-  uppercase(),
-);
-
-const lol = makeSomething("lol").pipe(
-  uppercase(),
-);
-
-const result2 = pipe(lol, map(s => `${s} - ${s}`));
-
-console.log({ result, lol });
+console.log(lol);
