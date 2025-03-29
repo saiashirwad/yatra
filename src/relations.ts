f38cd5140d54c8cbc5b65f7aca77e1eb75ad26ef
Sai Ashirwad <saiashirwadr@gmail.com>
2025-03-29 21:02:49 +0530

    eh

--- src/relations.ts
+++ src/relations.ts
@@ -1,10 +1,21 @@
-import {
-  ManyToManyRelation,
-  ManyToOneRelation,
-  OneToManyRelation,
-  OneToOneRelation,
-} from "./relation-types";
+import { Relation } from "./relation";
 import type { ExtractKeys, TableLike } from "./utils";
+
+export class OneToOneRelation<
+  S extends TableLike,
+  D extends TableLike,
+  FK extends string = string,
+  RK extends ExtractKeys<D> = ExtractKeys<D>,
+> extends Relation<S, D> {
+  constructor(
+    source: () => S,
+    destination: () => D,
+    public foreignKey: FK,
+    public referencedKey: RK = "id" as any,
+  ) {
+    super(source, destination);
+  }
+}
 
 export function oneToOne<
   S extends TableLike,
@@ -21,6 +32,22 @@ export function oneToOne<
     foreignKey,
     referencedKey,
   );
+}
+
+export class OneToManyRelation<
+  S extends TableLike,
+  D extends TableLike,
+  FK extends string = string,
+  RK extends ExtractKeys<D> = ExtractKeys<D>,
+> extends Relation<S, D> {
+  constructor(
+    source: () => S,
+    destination: () => D,
+    public foreignKey: FK,
+    public referencedKey: RK = "id" as any,
+  ) {
+    super(source, destination);
+  }
 }
 
 export function oneToMany<
@@ -40,6 +67,22 @@ export function oneToMany<
   );
 }
 
+export class ManyToOneRelation<
+  S extends TableLike,
+  D extends TableLike,
+  FK extends string = string,
+  RK extends ExtractKeys<D> = ExtractKeys<D>,
+> extends Relation<S, D> {
+  constructor(
+    source: () => S,
+    destination: () => D,
+    public foreignKey: FK,
+    public referencedKey: RK = "id" as any,
+  ) {
+    super(source, destination);
+  }
+}
+
 export function manyToOne<
   S extends TableLike,
   D extends TableLike,
@@ -55,6 +98,24 @@ export function manyToOne<
     foreignKey,
     referencedKey,
   );
+}
+
+export class ManyToManyRelation<
+  S extends TableLike,
+  D extends TableLike,
+  JT extends string = string,
+  SK extends string = string,
+  DK extends string = string,
+> extends Relation<S, D> {
+  constructor(
+    source: () => S,
+    destination: () => D,
+    public joinTable: JT,
+    public sourceKey: SK,
+    public destinationKey: DK,
+  ) {
+    super(source, destination);
+  }
 }
 
 export function manyToMany<
